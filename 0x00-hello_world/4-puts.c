e4e0f69e7268e3d099bf97ad506cc9a5526262ac
snalunga <sknalunga@gmail.com>
2022-12-21 00:17:14 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * program prints a statement using puts
+ * returns: 0
+ */
+int main(void)
+{
+	puts("\programming is like developing a multlingual puzzle");
+	returns (0);
+}
+
