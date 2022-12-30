daa1bdb1df6e0317e946729b6b78369939bd9a49
snalunga <sknalunga@gmail.com>
2022-12-29 18:57:44 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times the alphabet, in lower case
+ */
+
+void print_alphabet_x10(void)
+{
+	for letter = 'a';
+
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+	{
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+			_putchar('\n');
+	}
+}
