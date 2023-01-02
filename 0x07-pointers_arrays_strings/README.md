730f74a8d25786dc2cfdad88481824aae291af6e
snalunga <sknalunga@gmail.com>
2023-01-02 13:42:14 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is 0x07. C - Even more pointers, arrays and strings. will be saving all my tasks here.
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Sarah Nalunga
+ * description: Header file containing prototypes for all functions
+ * used in the 0x06-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
