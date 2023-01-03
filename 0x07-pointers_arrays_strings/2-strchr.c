4a539cbc02e96c73d0126d107b1dd9571367059f
snalunga <sknalunga@gmail.com>
2023-01-03 00:14:09 -0800

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: String
+ * @c: Character to search
+ * Return: pointer to the first occurance of the character
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c) 
+		{
+			return (s);
+		}
+		++s;
+		}
+	if (*s == c)
+	{
+		return (s);
+	}
