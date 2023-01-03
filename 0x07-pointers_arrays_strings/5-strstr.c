539ab51c4bd357442339e058c3e6f058d678a042
snalunga <sknalunga@gmail.com>
2023-01-03 01:20:19 -0800

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 
 	while (*haystack != '\0')
 	{
-		str1 = haystack'
+		str1 = haystack;
 		str2 = needle;
 
 		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
@@ -23,7 +23,7 @@ char *_strstr(char *haystack, char *needle)
 		}
 	if (*str2 == '\0')
 		return (str1);
-	heystack = str1 +1;
+	heystack = str1 + 1;
 	}
 	return (0);
 }
